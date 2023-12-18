944643cd43071045265b3a915cecde8a72c96305
Mark Nevison <152762127+mnevison@users.noreply.github.com>
2023-12-18 19:29:08 +0000

    Update finance_calculators.py

--- finance_calculators.py
+++ finance_calculators.py
@@ -1,20 +1,11 @@
 import math
 
-# outputting user friendly information about the program
-
 print('''
-
     Welcome to the Finance Calculator.
-      
-   Here you can provide us with details
-  of a bond or investment you are wanting
- to make and we will tell you your interest
-
-
+    Here you can provide us with details
+    of a bond or investment you are wanting
+    to make and we will tell you your interest
 ''')
-
-# getting information on the initial query 
-# adding safeguards with a while loop and booleans
 
 valid_choices = ["bond", "investment"]
 
@@ -23,49 +14,57 @@ while True:
     if choice in valid_choices:
         break
     else:
-        print("Invalid choice - Please select either Bond or Investnemt")
-
-# gathering the information needed for the investment calculations
+        print("Invalid choice - Please select either Bond or Investment")
 
 if choice == "investment":
-    total = float(input("Please tell us the total you would like to invest: "))
-    # using float allows the use of decimals in the rates added
-    rate = float(input("And at what interest rate is that? "))
-    time = int(input("And over how many years? "))
-
-# adding more safeguards
+    while True:
+        try:
+            total = float(input("Please tell us the total you would like to invest: "))
+            adjusted_rate = float(input("And at what interest rate is that? "))
+            rate = adjusted_rate / 100
+            time = int(input("And over how many years? "))
+            if total <= 0 or rate <= 0 or time <= 0:
+                print("Error - Please enter numerical value above 0")
+            else:
+                break
+        except ValueError:
+            print("Error - Only numerical values can be entered. Please try again")
 
     valid_choices = ["simple", "compound"]
 
     while True:
-        user_input = input("Now please tell us if you would like to see\
-that with simple or compound interest: ").lower()
+        user_input = input("Now please tell us if\
+ you would like to see that with simple or compound interest: ").lower()
         if user_input in valid_choices:
             break
         else:
             print("Invalid choice - Please select either simple or compound interest")
 
-# calculations for specified rates
-
+    # calculations for specified rates
     if user_input == "simple":
-        simple = (total * rate * time) / 100
-        print(f"£{simple} is the total after {time} years\
-with a simple interest rate of {rate}%")
-              
+        simple = total * (1 + rate * time)
+        print(f"£{simple:.2f} is the total after\
+ {time} years with a simple interest rate of {adjusted_rate}%")
+                  
     elif user_input == "compound":
-        compound_interest = total * (math.pow((1 + rate / 100), time))
-        print(f"£{compound_interest:.2f} is the total after {time} years\
-with a compound interest rate of {rate}%")
-
-# data and calculations for the bond selection
+        compound_interest = total * (math.pow((1 + rate), time))
+        print(f"£{compound_interest:.2f} is the total\
+ after {time} years with a compound interest rate of {adjusted_rate}%")
 
 elif choice == "bond":
-    value = float(input("Please tell us the current value of the house: "))
-    # Using float allows the use of decimals in the rates added
-    rate = float(input("What is the monthly interest rate? ")) 
-    months = int(input("And over how many months will the bond be repaid? "))
+    while True:
+        try:
+            value = float(input("Please tell us the current value of the house: "))
+            adjusted_rate = float(input("What is the monthly interest rate? "))
+            rate = adjusted_rate / 100 / 12
+            months = int(input("And over how many months will the bond be repaid? "))
+            if value <= 0 or rate <= 0 or months <= 0:
+                print("Error - Please enter numerical values above 0")
+            else:
+                break
+        except ValueError:
+            print("Error - Only numerical values can be entered - Please try again")
 
-    monthly_rate = rate / 100 / 12  # Converting annual rate to monthly rate
-    repayment = (monthly_rate * value) / (1 - (1 + monthly_rate) ** -months)
-    print(f"You will pay £{repayment:.2f} per month based on the\
-value of £{value} and interest rate of {rate}%")
+    repayment = (rate * value) / (1 - (1 + rate) ** -months)
+    print(f"You will pay £{repayment:.2f} per\
+ month based on the value of £{value} and interest rate of {adjusted_rate}%")
